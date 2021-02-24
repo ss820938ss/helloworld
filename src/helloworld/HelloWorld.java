ae654b3f88afddc735ed86d6e67ed039d9cf755e
parkyujin <ss820938ss@gmail.com>
2021-02-24 15:12:36 +0900

    main add

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,8 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("Hello World");
+		System.out.println("World's end Dancehall");
 
 	}
 
